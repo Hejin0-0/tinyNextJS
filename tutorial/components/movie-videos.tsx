71e73190524e7d3918f10ea55a375c95fe35c208
Hejin0-0 <lovenstein0624@gmail.com>
2024-04-03 14:03:15 +0900

    #4.2 Movie Styles

--- tutorial/components/movie-videos.tsx
+++ tutorial/components/movie-videos.tsx
@@ -1,8 +1,10 @@
 import { API_URL } from "../app/(home)/page";
 
 async function getVideos(id: string) {
-	console.log(`Fetching videos: ${Date.now()}`);
-	throw new Error("something broke...");
+	const response = await fetch(`${API_URL}/${id}/videos`);
+	return response.json();
+	// console.log(`Fetching videos: ${Date.now()}`);
+	// throw new Error("something broke...");
 	// 	await new Promise((resolve) => setTimeout(resolve, 3000));
 	// 	const response = await fetch(`${API_URL}/${id}/videos`);
 	// 	return response.json();
